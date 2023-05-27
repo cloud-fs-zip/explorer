ed0c1fb5e60d922582db8892e6cc88c2ca15dee0
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-27 11:36:00 +0200

    Create peer-connection-streams.js

---
+++ modules/peer-connection-streams.js
@@ -0,0 +1,12 @@
+// a collection of peer negotiated peer connection streams
+
+// auto negotiation bidirectional aka perfect negotiation can switch directions if needed.
+
+// remote is server first negotiation
+
+// i am server first negotiation
+
+// server negotiation
+
+// client negotiation
+
