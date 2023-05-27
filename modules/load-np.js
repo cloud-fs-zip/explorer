dac862eece0699b67b2db3c962fe8022d794950d
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-27 11:27:31 +0200

    Create load-np.js

---
+++ modules/load-np.js
@@ -0,0 +1,4 @@
+// Loads NPM managed packages as modules so that you can run and execute them. 
+// mainly loads the tgz and applys the npm hooks and returns handels to the files in it. 
+// additional it offers helpers to run scripts from the package.json and resolve
+// the correct imports. for dependency loading this uses the ./unpkg.js modules.
