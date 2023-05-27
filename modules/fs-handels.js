17bfa3059649bb703b0d8f967f85ae7c6240f5e5
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-27 11:16:59 +0200

    Create fs-handels.js

---
+++ modules/fs-handels.js
@@ -0,0 +1,2 @@
+// Interfaces as functions to turn anything into a Compatible fileSystemHandle. with the correct additional helper methods and api.
+// in case of native opfs it adds a stream based api.
